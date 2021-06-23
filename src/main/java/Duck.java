4c6644a07dd7cd584018957f3a6f4654792367b1
navadaniel <navita.gonzales@gmail.com>
2021-06-22 22:19:25 -0400

    We created of original structure

---
+++ src/main/java/Duck.java
@@ -0,0 +1,9 @@
+public abstract class Duck {
+	public void quack(){
+		System.out.println("quack");
+	}
+	public void swim(){
+		System.out.println("swim");
+	}
+	public abstract void display();
+}
---
+++ src/main/java/RedheadDuck.java
@@ -0,0 +1,6 @@
+public class RedheadDuck extends Duck{
+	@Override
+	public void display() {
+		System.out.println("I am a Readhead Duck!!");
+	}
+}
