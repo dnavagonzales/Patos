fff9d3b6b90f63199774cfdd6919fd85a8be938a
navadaniel <navita.gonzales@gmail.com>
2021-06-22 22:31:22 -0400

    I agree RubberDuck Class

---
+++ src/main/java/RubberDuck.java
@@ -0,0 +1,11 @@
+public class RubberDuck extends Duck{
+	@Override
+	public void quack() {
+		System.out.println("<<Squeak>>");
+	}
+
+	@Override
+	public void display() {
+		System.out.println("I am a Rubber Duck..");
+	}
+}
